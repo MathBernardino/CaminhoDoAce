4efa1a550eafe14b0f14030e2fc86eed8e86a5d0
MathBernardino <bernardinogomes310707@gmail.com>
2026-05-18 15:51:15 -0300

    FEAT Criação da funcionalidade de Login e Cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -19,20 +19,6 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
-
-                        // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                        //     .then((resultadoAquarios) => {
-                        //         if (resultadoAquarios.length > 0) {
-                        //             res.json({
-                        //                 id: resultadoAutenticar[0].id,
-                        //                 email: resultadoAutenticar[0].email,
-                        //                 nome: resultadoAutenticar[0].nome,
-                        //                 senha: resultadoAutenticar[0].senha
-                        //             });
-                        //         } else {
-                        //             res.status(204).json({ aquarios: [] });
-                        //         }
-                        //     })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -63,6 +49,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (nome.length < 3) {
+        res.status(400).send("Seu nome de usuário deve conter mais de 3 letras!");
+    } else if (senha.length < 6) {
+        res.status(400).send("Sua senha deve conter mais de 6 letras!");
     } else {
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nome, email, senha)
