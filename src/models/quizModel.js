57ea619879a461b712ce5345594311161338e85a
MathBernardino <bernardinogomes310707@gmail.com>
2026-05-22 20:56:13 -0300

    FEAT Modificação na funcionalidade de redirecionamento de páginas nas funcionalidades de login e cadastro

--- src/models/quizModel.js
+++ src/models/quizModel.js
@@ -1,11 +1,10 @@
 var database = require("../database/config")
 
-function cadastrar(resultado, usuarioID) {
-    console.log("Resultado:", resultado, "| UsuarioID:", usuarioID);
+function cadastrar(resultado, idUsuarioVincular) {
+    console.log("Resultado:", resultado, "| UsuarioID:", idUsuarioVincular);
 
     var instrucaoSql = `
-        UPDATE usuario SET posicao = '${resultado}' WHERE id = '${usuarioID}';
-    `;
+        UPDATE usuario SET posicao = '${resultado}' WHERE id = '${idUsuarioVincular}';`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
