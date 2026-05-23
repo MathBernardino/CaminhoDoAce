7eb01a9f1262c6ab9e236ec5ee654fd49709b3bf
MathBernardino <bernardinogomes310707@gmail.com>
2026-05-23 14:10:52 -0300

    FEAT Configuração da dashboardController

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,49 +1,31 @@
-var aquarioModel = require("../models/dashboard");
+var dashboardModel = require("../models/dashboardModel");
 
-function buscarAquariosPorEmpresa(req, res) {
-  var idUsuario = req.params.idUsuario;
-
-  aquarioModel.buscarAquariosPorEmpresa(idUsuario).then((resultado) => {
-    if (resultado.length > 0) {
-      res.status(200).json(resultado);
-    } else {
-      res.status(204).json([]);
-    }
-  }).catch(function (erro) {
-    console.log(erro);
-    console.log("Houve um erro ao buscar os aquarios: ", erro.sqlMessage);
-    res.status(500).json(erro.sqlMessage);
-  });
+function contarPerfis(req, res) {
+  dashboardModel.contarPerfis()
+    .then(resultado => res.json(resultado))
+    .catch(erro => {
+      console.log(erro);
+      res.status(500).json(erro.sqlMessage);
+    });
 }
 
 
-function cadastrar(req, res) {
-  var descricao = req.body.descricao;
-  var idUsuario = req.body.idUsuario;
-
-  if (descricao == undefined) {
-    res.status(400).send("descricao está undefined!");
-  } else if (idUsuario == undefined) {
-    res.status(400).send("idUsuario está undefined!");
-  } else {
-
-
-    aquarioModel.cadastrar(descricao, idUsuario)
-      .then((resultado) => {
-        res.status(201).json(resultado);
-      }
-      ).catch((erro) => {
-        console.log(erro);
-        console.log(
-          "\nHouve um erro ao realizar o cadastro! Erro: ",
-          erro.sqlMessage
-        );
-        res.status(500).json(erro.sqlMessage);
-      });
-  }
+function totalUsuarios(req, res) {
+  dashboardModel.contarUsuario()
+    .then(resultado => res.json(resultado))
+    .catch(erro => res.status(500).json(erro.sqlMessage));
 }
+
+
+function totalTentativas(req, res) {
+  dashboardModel.contarTentativas()
+    .then(resultado => res.json(resultado))
+    .catch(erro => res.status(500).json(erro.sqlMessage));
+}
+
 
 module.exports = {
-  buscarAquariosPorEmpresa,
-  cadastrar
+  contarPerfis,
+  contarTentativas,
+  contarUsuario
 }
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,14 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-var aquarioController = require("../controllers/dashboardController");
+var dashboardController = require("../controllers/dashboardController");
 
 router.get("/:empresaId", function (req, res) {
-  aquarioController.buscarAquariosPorEmpresa(req, res);
+  dashboardController.buscarAquariosPorEmpresa(req, res);
 });
-
-router.post("/cadastrar", function (req, res) {
-  aquarioController.cadastrar(req, res);
-})
 
 module.exports = router;
