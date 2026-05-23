754344c425628e57c3b0be2a8e63a91512c80a39
MathBernardino <bernardinogomes310707@gmail.com>
2026-05-23 14:16:52 -0300

    FEAT Configuração da dashboardController

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
 var quizRouter = require("./src/routes/quiz")
+var dashboardRouter = require("./src/routes/dashboard");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +29,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
+app.use("/dashboard", dashboardRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,10 +1,17 @@
 var express = require("express");
 var router = express.Router();
-
 var dashboardController = require("../controllers/dashboardController");
 
-router.get("/:empresaId", function (req, res) {
-  dashboardController.buscarAquariosPorEmpresa(req, res);
+router.get("/perfis", function (req, res) {
+    dashboardController.contarPerfis(req, res);
+});
+
+router.get("/usuarios", function (req, res) {
+    dashboardController.contarUsuario(req, res);
+});
+
+router.get("/tentativas", function (req, res) {
+    dashboardControllerr.contarTentativas(req, res);
 });
 
 module.exports = router;
