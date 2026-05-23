d9652d77011852e94a5303c4c2114e17c6f8f415
MathBernardino <bernardinogomes310707@gmail.com>
2026-05-22 22:14:06 -0300

    FEAT Funcionalidade do quiz adicionar o resultado no perfil do usuário feita

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -18,7 +18,7 @@ function autenticar(req, res) {
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
                     if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
+                        res.json(resultadoAutenticar[0]); 
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
